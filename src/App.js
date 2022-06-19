e3c58fac40f95b326208075c78cfcfbc63040da7
Fernando Goerl <fernandogoerl@gmail.com>
2022-06-18 19:28:57 -0600

    Improved game UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import flag from './icons/flag.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
+const difficulties = {
+  easy: {
+    rows: 9,
+    cols: 9,
+    mines: 15,
+  },
+  medium: {
+    rows: 16,
+    cols: 16,
+    mines: 40,
+  },
+  hard: {
+    rows: 16,
+    cols: 30,
+    mines: 99,
+  },
+};
+
 function App() {
   const [gameOn, setGameOn] = useState(false);
   const [gameBoard, setGameBoard] = useState([]);
@@ -17,6 +35,8 @@ function App() {
   const [showWarning, setShowWarning] = useState(false);
   const [timer, setTimer] = useState(0);
   const [timerStarted, setTimerStarted] = useState(false);
+  const [difficulty, setDifficulty] = useState('');
+  const [showCustomMenu, setShowCustomMenu] = useState(false);
 
   useEffect(() => {
     let timeCounter = null;
@@ -161,27 +181,6 @@ function App() {
     return board;
   };
 
-  const handleRowsChange = (event) => {
-    let maxRows = getMaxRows();
-    let rows =
-      +event.target.value < maxRows ? +event.target.value : Math.floor(maxRows);
-    setGameRows(rows);
-  };
-
-  const handleColsChange = (event) => {
-    let maxCols = getMaxCols();
-    let cols =
-      +event.target.value < maxCols ? +event.target.value : Math.floor(maxCols);
-    setGameCols(cols);
-  };
-
-  const handleMinesChange = (event) => {
-    let maxMines = gameRows > 0 && gameCols > 0 ? gameRows * gameCols - 1 : 0;
-    let mines = +event.target.value < maxMines ? +event.target.value : maxMines;
-    setMineCounter(mines);
-    setFlagCounter(mines);
-  };
-
   const addZeros = (number) => String(number).padStart(3, '0');
 
   const notOutOfBounds = (row, col) => {
@@ -321,10 +320,38 @@ function App() {
     !cell.hasMine && !timerStarted && setTimerStarted(true);
   };
 
-  const setGameDifficulty = (rows, cols, mines) => {
+  const handleDifficultyMenu = (difficulty = '') => {
+    setDifficulty(difficulty);
+  };
+
+  const handleRowsChange = (event) => {
+    let maxRows = getMaxRows();
+    let rows =
+      +event.target.value < maxRows ? +event.target.value : Math.floor(maxRows);
     setGameRows(rows);
+  };
+
+  const handleColsChange = (event) => {
+    let maxCols = getMaxCols();
+    let cols =
+      +event.target.value < maxCols ? +event.target.value : Math.floor(maxCols);
     setGameCols(cols);
+  };
+
+  const handleMinesChange = (event) => {
+    let maxMines = gameRows > 0 && gameCols > 0 ? gameRows * gameCols - 1 : 0;
+    let mines = +event.target.value < maxMines ? +event.target.value : maxMines;
     setMineCounter(mines);
+  };
+
+  const setGameDifficulty = (difficulty) => {
+    setDifficulty(difficulty);
+    if (difficulty !== 'custom') {
+      setGameRows(difficulties[difficulty].rows);
+      setGameCols(difficulties[difficulty].cols);
+      setMineCounter(difficulties[difficulty].mines);
+    } else {
+    }
   };
 
   const getMaxRows = () => (window.innerHeight - 369) / 40;
@@ -339,84 +366,137 @@ function App() {
       </header>
       <article>
         <div className='game-header'>
-          <div className='row custom-controls'>
-            <div className='input-group'>
-              <label>Rows</label>
-              <input
-                type='number'
-                name='rows'
-                id='rows'
-                className='control-border'
-                value={gameRows}
-                onChange={handleRowsChange}
-              />
+          {difficulty === 'customMenu' && (
+            <div className='custom-controls'>
+              <div className='row'>
+                <label>Rows</label>
+                <input
+                  type='number'
+                  name='rows'
+                  id='rows'
+                  className='control-border'
+                  value={gameRows}
+                  onChange={handleRowsChange}
+                />
+              </div>
+              <div className='row'>
+                <label>Cols</label>
+                <input
+                  type='number'
+                  name='cols'
+                  id='cols'
+                  className='control-border'
+                  value={gameCols}
+                  onChange={handleColsChange}
+                />
+              </div>
+              <div className='row'>
+                <label>Mines</label>
+                <input
+                  type='number'
+                  name='mines'
+                  id='mines'
+                  className='control-border'
+                  value={mineCounter}
+                  onChange={handleMinesChange}
+                />
+              </div>
             </div>
-            <div className='input-group'>
-              <label>Cols</label>
-              <input
-                type='number'
-                name='cols'
-                id='cols'
-                className='control-border'
-                value={gameCols}
-                onChange={handleColsChange}
-              />
-            </div>
-            <div className='input-group'>
-              <label>Mines</label>
-              <input
-                type='number'
-                name='mines'
-                id='mines'
-                className='control-border'
-                value={mineCounter}
-                onChange={handleMinesChange}
-              />
-            </div>
-          </div>
-          <div className='row difficulty-controls'>
-            <div
-              type='button'
-              className='btn control-border'
-              onClick={() => setGameDifficulty(9, 9, 5)}
-            >
-              Easy
-            </div>
-            <div
-              type='button'
-              className='btn control-border'
-              onClick={() => setGameDifficulty(16, 16, 40)}
-            >
-              Medium
-            </div>
-            <div
-              type='button'
-              className='btn control-border'
-              onClick={() => setGameDifficulty(16, 30, 99)}
-            >
-              Hard
-            </div>
+          )}
+          <div className='row'>
+            <div className='col'></div>
+            {!difficulty && (
+              <div
+                type='button'
+                className='btn control-border'
+                onClick={() => handleDifficultyMenu('menu')}
+              >
+                Select Difficulty
+              </div>
+            )}
+            {difficulty !== 'menu' && difficulty !== 'customMenu' && (
+              <div
+                type='button'
+                className='btn control-border'
+                onClick={() =>
+                  difficulty === 'menu'
+                    ? setGameDifficulty('easy')
+                    : handleDifficultyMenu('menu')
+                }
+              >
+                {` Difficulty: ${
+                  difficulty.charAt(0).toUpperCase() + difficulty.slice(1)
+                }`}
+              </div>
+            )}
+            {difficulty === 'menu' && (
+              <div>
+                <div
+                  type='button'
+                  className='btn control-border'
+                  onClick={() => setGameDifficulty('easy')}
+                >
+                  Easy
+                </div>
+                <div
+                  type='button'
+                  className='btn control-border'
+                  onClick={() => setGameDifficulty('medium')}
+                >
+                  Medium
+                </div>
+                <div
+                  type='button'
+                  className='btn control-border'
+                  onClick={() => setGameDifficulty('hard')}
+                >
+                  Hard
+                </div>
+                <div
+                  type='button'
+                  className='btn control-border'
+                  onClick={() => handleDifficultyMenu('customMenu')}
+                >
+                  Custom
+                </div>
+              </div>
+            )}
+            {difficulty === 'customMenu' && (
+              <div
+                type='button'
+                className='btn control-border'
+                onClick={() => setGameDifficulty('custom')}
+              >
+                Save
+              </div>
+            )}
+            <div className='col'></div>
           </div>
           <div className='warning'>
             {showWarning ? 'Fill all 3 fields if you want to play!' : ''}
           </div>
-          <div className='row'>
-            <div className='mines counter'>{addZeros(flagCounter)}</div>
-            <div
-              type='button'
-              className='btn new-game-btn control-border'
-              onClick={initGame}
-            >
-              New Game
+          {difficulty !== 'menu' && (
+            <div className='row'>
+              <div className='mines counter'>{addZeros(flagCounter)}</div>
+              <div
+                type='button'
+                className='btn new-game-btn control-border'
+                onClick={initGame}
+              >
+                New Game
+              </div>
+              <div className='timer counter'>{addZeros(timer)}</div>
             </div>
-            <div className='timer counter'>{addZeros(timer)}</div>
-          </div>
+          )}
         </div>
         <div className={`game-body ${gameOn && 'show-game'}`}>
           {gameBoard.map((row, index) => (
             <div className='row game-row' key={'row_' + index}>
               {row.map((cell) => (
-                <div className='col' key={'row_' + index + '__col_' + cell.col}>
+                <div
+                  className='cell'
+                  key={'row_' + index + '__col_' + cell.col}
+                >
                   {!cell.isOpen && (
                     <div
                       className={`closed-cell ${
